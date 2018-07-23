5cdf3d1c720641f6e56aaf129f28574b66228be8
Umpoy <iumpoy@yahoo.com>
2018-07-23 15:28:16 -0700

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# fullstack_sgt
+# Student Grade Table
+
+## Technologies: HTML, CSS, Bootstrap, Javascript, jQuery, PHP, MySQL
+
+## Description
+My Students Grade is an application developed to allow users to create an profile then create students, courses, and their student's grades. Some cool features include a single form for adding and updating students, courses, and grades. All user data for students, courses, assignments and grades is held in a SQL database and accessed through the PHP. The application was developed using bootstrap for a mobile responsive experience.
--- form.php
+++ form.php
@@ -3,8 +3,8 @@
     <h1 class="animated fadeInDown">Student Grade Table</h1>
     <div> <?php echo $error; ?> </div>
     <form method="post" id="signup" autocomplete="off" class="animated fadeInUp">
-        <input type="text" name="username" placeholder=" Enter Username" class="form-control">
-        <input type="password" name="password" placeholder="Enter Password" class="form-control">
+        <input type="text" name="username" placeholder=" Enter Username" class="form-control" pattern=".{3,12}" required title="3 to 12 characters">
+        <input type="password" name="password" placeholder="Enter Password" class="form-control" pattern=".{8}" required title="(8 chars minimum)">
         <input type="submit" name="signup" value="Sign Up" class="btn btn-success">
         <p>Already Signed Up? <a class="toggle btn btn-info">Log In</a></p>
     </form>
