1d35b08ac0f09d6690a423f925db08510212eaab
Umpoy <iumpoy@yahoo.com>
2018-07-23 14:20:39 -0700

    removing connect to server

--- connect.php
+++ connect.php
@@ -1,5 +1,5 @@
 <?php
-$conn = mysqli_connect('localhost','root', 'root', '');
+$conn = mysqli_connect('localhost','root', 'root', 'SGT');
 if (mysqli_connect_error()) {
         die ("Database Connection Error");
 }
