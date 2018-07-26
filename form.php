82017b3b2497092b3be684b53a73c453138350a9
Umpoy <iumpoy@yahoo.com>
2018-07-26 12:26:00 -0700

    added better restriction to form

--- form.php
+++ form.php
@@ -3,8 +3,8 @@
     <h1 class="animated fadeInDown">Student Grade Table</h1>
     <div> <?php echo $error; ?> </div>
     <form method="post" id="signup" autocomplete="off" class="animated fadeInUp">
-        <input type="text" name="username" placeholder=" Enter Username" class="form-control" pattern=".{3,12}" required title="3 to 12 characters">
-        <input type="password" name="password" placeholder="Enter Password" class="form-control" pattern=".{8}" required title="(8 chars minimum)">
+        <input type="text" name="username" placeholder=" Enter Username" class="form-control" required pattern="[a-zA-Z0-9_-]{3,12}" title="must be alphanumeric in 3-12 chars">
+        <input type="password" name="password" placeholder="Enter Password" class="form-control" ppattern=".{8,}"   required title="8 characters minimum" required title="(8 chars minimum)">
         <input type="submit" name="signup" value="Sign Up" class="btn btn-success">
         <p>Already Signed Up? <a class="toggle btn btn-info">Log In</a></p>
     </form>
